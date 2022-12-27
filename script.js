84273f54793ae6882dc18b8ea5414efd1eb09c3e
Ayush Agarwal <ayush_a1@ch.iitr.ac.in>
2022-12-27 19:14:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//jshint esversion:6
+
+//========For sticky Navbar on scrolling===========
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+//==========Typing Animation========
+var typed = new Typed(".typing",{
+    strings:["Web Developer.", "Competitive Programmer.", "Designer.", "Musician.", "IITian."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+
+//=========Menu-Icon For small width screen (Responsive)=========
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
